811fe0d3716a228d6fffab202c8687a2be966d25
Meco Man <34888354+mysterywolf@users.noreply.github.com>
2020-09-06 23:02:09 +0800

    Update ioctl.c

--- ioctl.c
+++ ioctl.c
@@ -16,8 +16,10 @@ int stdout_ioctl (int __fd, unsigned long int __request, void *args)
     struct winsize* p_winsize;
 
     if(__fd != STDOUT_FILENO)
+    {
         errno = ENOTTY;
         return -1;
+    }
 
     p_winsize = (struct winsize*)args;
 
